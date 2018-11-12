51cc5a3625aaa40b0f3d81b56df12485ca00cbd0
Aleksandr Bobyr <alex.tachcard911@gmail.com>
2018-11-12 11:58:26 +0200

    added dj_database_url

--- insurance/settings.py
+++ insurance/settings.py
@@ -149,3 +149,7 @@ STATICFILES_DIRS = (
 )
 
 django_heroku.settings(locals())
+
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)

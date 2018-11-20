788e8941942e80308365dbbbc0fb664bff176dd8
Aleksandr Bobyr <alex.tachcard911@gmail.com>
2018-11-20 15:10:53 +0200

    added media url and root

--- insurance/urls.py
+++ insurance/urls.py
@@ -23,4 +23,4 @@ from .routers import router
 urlpatterns = [
     path('api/', include(router.urls)),
     path('admin/', admin.site.urls),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
